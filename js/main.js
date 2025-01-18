08d56232a8e707e62e6c24e8be6ff329dd03fc85
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-01-18 22:31:56 +0200

    adapt info section

--- js/main.js
+++ js/main.js
@@ -2,7 +2,16 @@ $(function () {
     $('.slider-box').slick({
         fade: true,
         prevArrow: '<img class="slider-arrow slider-arrow-prev" src="images/icon-arrow-prev.svg" alt="arrow-prev" >',
-        nextArrow: '<img class="slider-arrow slider-arrow-next" src="images/icon-arrow-next.svg" alt="arrow-next" >'
+        nextArrow: '<img class="slider-arrow slider-arrow-next" src="images/icon-arrow-next.svg" alt="arrow-next" >',
+        responsive: [
+            {
+              breakpoint: 641,
+              settings: {
+                arrows: false
+              }
+            }
+        
+        ]
     });
 
     $('.menu-btn').on('click', function () {
