d459f773a59860dd2d502c665915a421797bc226
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-01-18 13:26:24 +0200

    add styles to the info section slider block

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('.slider-box').slick({
+        fade: true,
+        prevArrow: '<img class="slider-arrow slider-arrow-prev" src="images/icon-arrow-prev.svg" alt="arrow-prev" >',
+        nextArrow: '<img class="slider-arrow slider-arrow-next" src="images/icon-arrow-next.svg" alt="arrow-next" >'
+    });
+ });
